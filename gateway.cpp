b6de0ab1d96e3b314d99b9669c1ad50e7859691d
Denis Obukhov <denysobukhov@gmail.com>
2020-12-10 13:11:11 +0200

    removed node name added message metadata

--- gateway.cpp
+++ gateway.cpp
@@ -23,24 +23,27 @@
 RH_RF95 rf95(RF_CS_PIN, RF_IRQ_PIN);
 
 struct Message {
-	char id[8];
 	int32_t pressure;
 	int32_t humidity;
 	int32_t temperature;
 	int32_t voltage;
 } msg;
 
-const std::string currentDateTime() {
-	time_t     now = time(0);
-	struct tm  tstruct;
-    	char       buf[80];
-    	tstruct = *localtime(&now);
-    	strftime(buf, sizeof(buf), "%Y-%m-%d %X %z", &tstruct);
-    	return buf;
-}
+float freq;
+int port;
+std::string client_id, host, topic;
 
 //Flag for Ctrl-C
 volatile sig_atomic_t force_exit = false;
+
+const std::string currentDateTime() {
+	time_t     now = time(0);
+	struct tm  tstruct;
+	char       buf[80];
+	tstruct = *localtime(&now);
+	strftime(buf, sizeof(buf), "%Y-%m-%d %X %z", &tstruct);
+	return buf;
+}
 
 void signal_handler(int sig_num){
 	printf("\n%s Break received, exiting!\n", __BASEFILE__);
@@ -72,19 +75,17 @@ int main(int argc, char** argv) {
 	}
 	catch (const libconfig::FileIOException &fioex)
 	{
-		std::cerr << "I/O error while reading file rfm24-mqtt-gateway.cfg" << std::endl;
+		std::cerr << "I/O error while reading file gateway.cfg" << std::endl;
+		syslog (LOG_INFO, "I/O error while reading file gateway.cfg");
 		return(EXIT_FAILURE);
 	}
 	catch (const libconfig::ParseException &pex)
 	{
 		std::cerr << "Parse error at " << pex.getFile() << ":" << pex.getLine()
-			<< " - " << pex.getError() << std::endl;
+		<< " - " << pex.getError() << std::endl;
 		return(EXIT_FAILURE);
 	}
 
-	float freq;
-	int port;
-	std::string client_id, host, topic;
 	try
 	{
 		freq = cfg.lookup("radio_freq");
@@ -111,25 +112,25 @@ int main(int argc, char** argv) {
 		fprintf( stderr, "%s bcm2835_init() Failed\n\n", __BASEFILE__ );
 		return 1;
 	}
-        printf( "RF95 CS=GPIO%d", RF_CS_PIN);
+	printf( "RF95 CS=GPIO%d", RF_CS_PIN);
 
 	#ifdef RF_IRQ_PIN
-  		printf( ", IRQ=GPIO%d", RF_IRQ_PIN );
-  		// IRQ Pin input/pull down
-  		pinMode(RF_IRQ_PIN, INPUT);
-  		bcm2835_gpio_set_pud(RF_IRQ_PIN, BCM2835_GPIO_PUD_DOWN);
-  		// Now we can enable Rising edge detection
-  		bcm2835_gpio_ren(RF_IRQ_PIN);
+	printf( ", IRQ=GPIO%d", RF_IRQ_PIN );
+	// IRQ Pin input/pull down
+	pinMode(RF_IRQ_PIN, INPUT);
+	bcm2835_gpio_set_pud(RF_IRQ_PIN, BCM2835_GPIO_PUD_DOWN);
+	// Now we can enable Rising edge detection
+	bcm2835_gpio_ren(RF_IRQ_PIN);
 	#endif
-  
+
 	#ifdef RF_RST_PIN
-		printf( ", RST=GPIO%d", RF_RST_PIN );
-		// Pulse a reset on module
-		pinMode(RF_RST_PIN, OUTPUT);
-		digitalWrite(RF_RST_PIN, LOW );
-		bcm2835_delay(150);
-		digitalWrite(RF_RST_PIN, HIGH );
-		bcm2835_delay(100);
+	printf( ", RST=GPIO%d", RF_RST_PIN );
+	// Pulse a reset on module
+	pinMode(RF_RST_PIN, OUTPUT);
+	digitalWrite(RF_RST_PIN, LOW );
+	bcm2835_delay(150);
+	digitalWrite(RF_RST_PIN, HIGH );
+	bcm2835_delay(100);
 	#endif
 
 	std::cout << "\nRadio initialized." << std::endl;
@@ -157,134 +158,133 @@ int main(int argc, char** argv) {
 		fprintf( stderr, "\nRF95 module init failed, Please verify wiring/module\n" );
 		return -1;
 	}
-    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+	// Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
 
-    // The default transmitter power is 13dBm, using PA_BOOST.
-    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
-    // you can set transmitter powers from 5 to 23 dBm:
-    //  driver.setTxPower(23, false);
-    // If you are using Modtronix inAir4 or inAir9,or any other module which uses the
-    // transmitter RFO pins and not the PA_BOOST pins
-    // then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true. 
-    // Failure to do that will result in extremely low transmit powers.
-    // rf95.setTxPower(14, true);
+	// The default transmitter power is 13dBm, using PA_BOOST.
+	// If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
+	// you can set transmitter powers from 5 to 23 dBm:
+	//  driver.setTxPower(23, false);
+	// If you are using Modtronix inAir4 or inAir9,or any other module which uses the
+	// transmitter RFO pins and not the PA_BOOST pins
+	// then you can configure the power transmitter power for -1 to 14 dBm and with useRFO true.
+	// Failure to do that will result in extremely low transmit powers.
+	// rf95.setTxPower(14, true);
 
 
-    // RF95 Modules don't have RFO pin connected, so just use PA_BOOST
-    // check your country max power useable, in EU it's +14dB
-    rf95.setTxPower(14, false);
+	// RF95 Modules don't have RFO pin connected, so just use PA_BOOST
+	// check your country max power useable, in EU it's +14dB
+	rf95.setTxPower(14, false);
 
-    // You can optionally require this module to wait until Channel Activity
-    // Detection shows no activity on the channel before transmitting by setting
-    // the CAD timeout to non-zero:
-    //rf95.setCADTimeout(10000);
+	// You can optionally require this module to wait until Channel Activity
+	// Detection shows no activity on the channel before transmitting by setting
+	// the CAD timeout to non-zero:
+	//rf95.setCADTimeout(10000);
 
-    // Adjust Frequency
-    rf95.setFrequency(freq);
-    
-    // If we need to send something
-    
-    //rf95.setThisAddress(RF_NODE_ID);
-    //rf95.setHeaderFrom(RF_NODE_ID);
-    
-    // Be sure to grab all node packet 
-    // we're sniffing to display, it's a demo
-    rf95.setPromiscuous(true);
+	// Adjust Frequency
+	rf95.setFrequency(freq);
 
-//        Bw125Cr45Sf128 = 0,        ///< Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Default medium range
-//        Bw500Cr45Sf128,            ///< Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range
-//        Bw31_25Cr48Sf512,          ///< Bw = 31.25 kHz, Cr = 4/8, Sf = 512chips/symbol, CRC on. Slow+long range
-//        Bw125Cr48Sf4096,           ///< Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. Slow+long range
+	// If we need to send something
 
-    rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
-    rf95.setPreambleLength(8);
+	//rf95.setThisAddress(RF_NODE_ID);
+	//rf95.setHeaderFrom(RF_NODE_ID);
 
-    // We're ready to listen for incoming message
-    rf95.setModeRx();
+	// Be sure to grab all node packet
+	// we're sniffing to display, it's a demo
+	rf95.setPromiscuous(true);
 
-    //printf( " OK NodeID=%d @ %3.2fMHz\n", RF_NODE_ID, RF_FREQUENCY );
-    //printf( "Listening packet...\n" );
+	//        Bw125Cr45Sf128 = 0,        ///< Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Default medium range
+	//        Bw500Cr45Sf128,            ///< Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range
+	//        Bw31_25Cr48Sf512,          ///< Bw = 31.25 kHz, Cr = 4/8, Sf = 512chips/symbol, CRC on. Slow+long range
+	//        Bw125Cr48Sf4096,           ///< Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. Slow+long range
 
-    //Begin the main body of code
+	rf95.setModemConfig(RH_RF95::Bw500Cr45Sf128);
+	rf95.setPreambleLength(8);
+
+	// We're ready to listen for incoming message
+	rf95.setModeRx();
+
+	syslog (LOG_INFO, "Radio OK NodeID = %d Listening @ %3.2fMHz\n", RF_NODE_ID, RF_FREQUENCY);
+
+	//Begin the main body of code
 	while (!force_exit)
 	{
 
-#ifdef RF_IRQ_PIN
-      // We have a IRQ pin ,pool it instead reading
-      // Modules IRQ registers from SPI in each loop
-      
-      // Rising edge fired ?
-      if (bcm2835_gpio_eds(RF_IRQ_PIN)) {
-        // Now clear the eds flag by setting it to 1
-        bcm2835_gpio_set_eds(RF_IRQ_PIN);
-        //printf("Packet Received, Rising event detect for pin GPIO%d\n", RF_IRQ_PIN);
-#endif
+		#ifdef RF_IRQ_PIN
+		// We have a IRQ pin ,pool it instead reading
+		// Modules IRQ registers from SPI in each loop
 
-        if (rf95.available()) { 
-          // Should be a message for us now
-          uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-          uint8_t len  = sizeof(buf);
-          uint8_t from = rf95.headerFrom();
-          uint8_t to   = rf95.headerTo();
-          uint8_t id   = rf95.headerId();
-          uint8_t flags= rf95.headerFlags();;
-          int8_t rssi  = rf95.lastRssi();
-          
-          if (rf95.recv(buf, &len)) {
-			  
-			if (sizeof(msg) == len) {
-				memcpy(&msg, buf, len);
-				printf("Got payload size=%i:\t t=%i, h=%i, p=%i, v=%i \r\n", len, msg.temperature, msg.humidity, msg.pressure, msg.voltage);
-				try
-			{
-				std::ostringstream ostr;
-				ostr << "<message time=\"" << currentDateTime() << "\">";
-				ostr << "<temperature>" << msg.temperature / 10.0 << "</temperature>";
-				ostr << "<humidity>" << msg.humidity / 10.0 << "</humidity>";
-				ostr << "<pressure>" << msg.pressure << "</pressure>";
-				ostr << "<voltage>" << msg.voltage / 100.0 << "</voltage>";
-				ostr << "</message>";
-				std::string m = ostr.str();
-				//std::cout << m.size() << ":" << m << std::endl;
-				int r = mqtt_->send(topic.c_str(), m.c_str(), m.size());
-				if (r != MOSQ_ERR_SUCCESS)
-				{
-					std::cout << "Message sending error: " << mosqpp::strerror(r) << std::endl;
-					mqtt_->reconnect();
-					continue;
+		// Rising edge fired ?
+		if (bcm2835_gpio_eds(RF_IRQ_PIN)) {
+			// Now clear the eds flag by setting it to 1
+			bcm2835_gpio_set_eds(RF_IRQ_PIN);
+			//printf("Packet Received, Rising event detect for pin GPIO%d\n", RF_IRQ_PIN);
+			#endif
+
+			if (rf95.available()) {
+				// Should be a message for us now
+				uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+				uint8_t len  = sizeof(buf);
+				uint8_t from = rf95.headerFrom();
+				uint8_t to   = rf95.headerTo();
+				uint8_t id   = rf95.headerId();
+				uint8_t flags= rf95.headerFlags();;
+				int8_t rssi  = rf95.lastRssi();
+
+				if (rf95.recv(buf, &len)) {
+
+					if (sizeof(msg) == len) {
+						memcpy(&msg, buf, len);
+						printf("Got payload size=%i:\t t=%i, h=%i, p=%i, v=%i \r\n", len, msg.temperature, msg.humidity, msg.pressure, msg.voltage);
+						try
+						{
+							std::ostringstream ostr;
+							ostr << "<message time=\"" << currentDateTime() << "\" from=\"" << from << "\" to=\"" << to << "\" rssi=\"" << rssi << "\" id=\"" << id << "\" >";
+							ostr << "<temperature>" << msg.temperature / 10.0 << "</temperature>";
+							ostr << "<humidity>" << msg.humidity / 10.0 << "</humidity>";
+							ostr << "<pressure>" << msg.pressure << "</pressure>";
+							ostr << "<voltage>" << msg.voltage / 100.0 << "</voltage>";
+							
+							ostr << "</message>";
+							std::string m = ostr.str();
+
+							int i = 1, r;
+							do {
+								r = mqtt_->send(topic.c_str(), m.c_str(), m.size());
+							  if (r != MOSQ_ERR_SUCCESS) {
+									printf("Message sending error (retry #%d) : %s\n", i, mosqpp::strerror(r));
+									mqtt_->reconnect();
+								}
+							} while (r != MOSQ_ERR_SUCCESS && i++ < 3)
+						}
+						catch (int e) {
+							std::cout << "mqtt_send exception " << e << std::endl;
+							return(EXIT_FAILURE);
+						}
+
+					}
+					else {
+						printf("unknown payload size=%i\n\r", len);
+					}
+
+
+					//printf("From: [#%d]", to);
+					printf("Packet id: %d, len = %02d, from: %d, to: #%d, rssi: %ddB:\n", id, len, from, to, rssi);
+					printbuffer(buf, len);
+
+				} else {
+					printf("receive failed");
 				}
-
-			}
-			catch (int e) {
-				std::cout << "mqtt_send exception " << e << std::endl;
-				return(EXIT_FAILURE);
+				printf("\n");
 			}
 
-			}
-			else {
-				printf("unknown payload size=%i\n\r", len);
-			}			  
-			  
-			  
-            //printf("From: [#%d]", to);
-            printf("Packet id: %d, len = %02d, from: %d, to: #%d, rssi: %ddB:\n", id, len, from, to, rssi);
-            printbuffer(buf, len);
-            
-          } else {
-            printf("receive failed");
-          }
-          printf("\n");
-        }
-        
-#ifdef RF_IRQ_PIN
-      }
-#endif
-     
+			#ifdef RF_IRQ_PIN
+		}
+		#endif
+
 		bcm2835_delay(5);
 	}
-	
+
 	printf( "\n%s Ending\n", __BASEFILE__ );
 	bcm2835_close();
 	return 0;
 }
-
